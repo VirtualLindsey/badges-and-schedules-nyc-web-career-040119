2d643c2f936041ed4118b0c149f17e82f801f8e2
L D <tapircode@gmail.com>
2019-03-12 02:45:18 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end 
+
+names = ["sam", "max", "zagrazeb", "san pedro", "ba'al", "rosalind"]
+
+batch_badge_creator(names)
+
+def batch_badge_creator(array)
+  result = []
+  array.each_with_index do |i, j|
+    puts badge_maker(i)
+end
+  
+end 
+
+def assign_rooms
+end
+
+def printer
+end
+
