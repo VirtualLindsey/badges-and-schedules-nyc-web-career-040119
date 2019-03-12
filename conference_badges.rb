9d43a0411f7e26e730adaadd71fbdaf32700421b
L D <tapircode@gmail.com>
2019-03-12 02:51:00 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,10 +2,6 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end 
 
-names = ["sam", "max", "zagrazeb", "san pedro", "ba'al", "rosalind"]
-
-batch_badge_creator(names)
-
 def batch_badge_creator(array)
   result = []
   array.each_with_index do |name|
